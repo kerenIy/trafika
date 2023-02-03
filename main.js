06496cab76f01f530e3ff6901b53ed0631e61904
tein <92306279+kerenIy@users.noreply.github.com>
2023-02-03 09:18:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ function getFlight() {
     .then((data) => console.log(data))
     .catch((err) => console.error(err));
 }
+
+
+const hotels = document.getElementById("hotel");
+
+flights.addEventListener("click", getHotel);
+
+function getHotel() {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "0c61322343mshe5386bc17e61688p19e553jsnd4d160510343",
+      "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=eiffel%20tower&lang=en_US&units=km",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+}
